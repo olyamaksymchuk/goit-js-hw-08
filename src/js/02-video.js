9cd1d102b9b0bc9792e49e85aeb3dce12930d126
olyamaksymchuk <109147843+olyamaksymchuk@users.noreply.github.com>
2023-03-13 12:18:01 +0200

    uploaded

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function (data) {
+    localStorage.setItem ('videoplayer-current-time', JSON.stringify(data.seconds));
+        let time=Number(localStorage.setItem('videoplayer-current-time'));
+        console.log(time);
+    }
+player.on('play', onPlay);
+
+player.setCurrentTime(30).then(function(seconds) {
+
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+default:
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+   form: document.querySelector('.feedback-form'),
+   textarea: document.querySelector('.feedback-form textarea'),
+   email: document.querySelector('.feedback-form input'),
+};
+
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+const savedData = localStorage.getItem(STORAGE_KEY, JSON.stringify(formData));
+const parsedData = JSON.parse(savedData);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+populateTextarea();
+
+function onInput(evt) {
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+ 
+    formData[evt.target.name] = evt.target.value;
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    
+    if (refs.email.value === '' || refs.textarea.value === '') {
+        alert('Please fill in all the fields!');
+    }
+    console.log(formData);
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateTextarea(evt) {
+    const savedLocalStorage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedLocalStorage) {
+        refs.form.email.value = parsedData.email;
+        refs.form.message.value = parsedData.message;
+    }
+
+    function populateTextarea() {
+
+    }
+   
+ };
